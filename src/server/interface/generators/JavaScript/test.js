262d50c8edcb19c29b4fbdbe122858ae7cfcf860
Paulo Trentin <paulo@paulotrentin.com.br>
2015-02-14 19:40:49 -0200

    Ajustes nova interface para manipulação de palca Início de geração de código para blocos personalizados fixes #5

---
+++ src/server/interface/generators/JavaScript/test.js
@@ -0,0 +1,9 @@
+Blockly.JavaScript['relay_board'] = function (block) {
+    var argument1 = Blockly.JavaScript.valueToCode(block, 'VALUE',
+            Blockly.JavaScript.ORDER_MEMBER) || '\'\'';
+
+    var value = (argument1 == 'true') ? true : false;
+    var code = 'setRelay("' + block.getFieldValue('NAME') + '","' + value + '");\n';
+
+    return code;
+};
