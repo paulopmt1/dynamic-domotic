a503558493b44927a7bc2ce0ffaae644e199f9a3
Paulo Trentin <paulo@paulotrentin.com.br>
2015-02-16 17:39:34 -0200

    Organização do código e início suporte dinâmico no menu dropdown #10

---
+++ src/server/interface/block/relay_board.js
@@ -0,0 +1,19 @@
+// Código https://blockly-demo.appspot.com/static/demos/blockfactory/index.html#j4gf7n
+Blockly.Blocks['relay_board'] = {
+    init: function () {
+        this.setHelpUrl('http://www.example.com/');
+        this.setColour(20);
+        this.appendDummyInput()
+                .appendField("Defina")
+                .appendField(new Blockly.FieldDropdown([["Placa Garagem", "23AD"], ["Placa Quarto", "18BC"]]), "board_id")
+                .appendField(" .")
+                .appendField(new Blockly.FieldDropdown([["Atuador 1", "RELAY_1"], ["Atuador 2", "RELAY_2"], ["Atuador 3", "RELAY_3"]]), "NAME")
+                .appendField(". Para");
+        this.appendValueInput("VALUE")
+                .setCheck(["Boolean", "Number"]);
+        this.setInputsInline(true);
+        this.setPreviousStatement(true);
+        this.setNextStatement(true);
+        this.setTooltip('');
+    }
+};
--- src/server/interface/generators/JavaScript/relay_board.js
+++ src/server/interface/generators/JavaScript/relay_board.js
@@ -1,9 +1,8 @@
 Blockly.JavaScript['relay_board'] = function (block) {
     var argument1 = Blockly.JavaScript.valueToCode(block, 'VALUE',
             Blockly.JavaScript.ORDER_MEMBER) || '\'\'';
-
-    var value = (argument1 == 'true') ? true : false;
-    var code = 'setRelay("' + block.getFieldValue('board_id') + '","' + block.getFieldValue('NAME') + '","' + value + '");\n';
+            
+    var code = 'setRelay("' + block.getFieldValue('board_id') + '","' + block.getFieldValue('NAME') + '","' + argument1 + '");\n';
 
     return code;
 };
