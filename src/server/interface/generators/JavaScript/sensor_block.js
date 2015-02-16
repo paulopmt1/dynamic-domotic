8af99ae23ae8fee2aff644bf36a4911791ae8743
Paulo Trentin <paulo@paulotrentin.com.br>
2015-02-15 22:31:40 -0200

    Melhoria geração do código para manipular evento de uma placa #7

--- src/server/interface/generators/JavaScript/sensor_block.js
+++ src/server/interface/generators/JavaScript/sensor_block.js
@@ -2,8 +2,12 @@ Blockly.JavaScript['sensor_block'] = function (block) {
     var boardId = block.getFieldValue('board_id');
     var sensorId = block.getFieldValue('sensor_id');
     
-    var test = Blockly.JavaScript.statementToCode(block, 'DO_SOMETHING');
-    var code = 'callX = function(boardId, sensorId){\n' + test + '}\("' + boardId + '", "' + sensorId + '");\n';
+    var internalCode = Blockly.JavaScript.statementToCode(block, 'DO_SOMETHING');
+    
+    var code = "$.subscribe('handleBoard/" + boardId + "/" + sensorId + "', function(){\n"
+        + internalCode + "\})";
+    
+    //var code = 'callX = function(boardId, sensorId){\n' + test + '}\("' + boardId + '", "' + sensorId + '");\n';
 
     return code;
 };
