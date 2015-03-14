7f70b6cfbcf8260efda757c6e725eb39dee1da2e
Paulo Trentin <paulo@paulotrentin.com.br>
2015-03-13 21:40:52 -0300

    Salvando trabalho do workspace no sistema de arquivos local #13

---
+++ src/server/interface/system/storage.js
@@ -0,0 +1,33 @@
+// Create a namespace.
+var BlocklyStorage = {};
+
+/**
+ * Backup code blocks to localStorage.
+ * @private
+ */
+BlocklyStorage.backupBlocks_ = function () {
+    if ('localStorage' in window) {
+        var xml = Blockly.Xml.workspaceToDom(Blockly.getMainWorkspace());
+        // Gets the current URL, not including the hash.
+        var url = window.location.href.split('#')[0];
+        window.localStorage.setItem(url, Blockly.Xml.domToText(xml));
+    }
+};
+
+/**
+ * Bind the localStorage backup function to the unload event.
+ */
+BlocklyStorage.backupOnUnload = function () {
+    window.addEventListener('unload', BlocklyStorage.backupBlocks_, false);
+};
+
+/**
+ * Restore code blocks from localStorage.
+ */
+BlocklyStorage.restoreBlocks = function () {
+    var url = window.location.href.split('#')[0];
+    if ('localStorage' in window && window.localStorage[url]) {
+        var xml = Blockly.Xml.textToDom(window.localStorage[url]);
+        Blockly.Xml.domToWorkspace(Blockly.getMainWorkspace(), xml);
+    }
+};
